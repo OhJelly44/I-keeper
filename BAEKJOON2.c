e29e9beb9501976f1f5115279e5796c15ccebc10
OhJelly44 <j1004zoo@cu.ac.kr>
2024-03-27 16:23:58 +0900

    백준과제 1일차

---
+++ BAEKJOON2.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+/*int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n%d\n%d\n%d\n%d", a+b, a-b, a*b, a/b, a%b);
+    return 0;
+}*/
+
+/*
+int main()
+{
+    int a, b;
+
+    scanf_s("%d %d", &a, &b);
+    printf("%d\n", a + b);
+    printf("%d\n", a - b);
+    printf("%d\n", a * b);
+    printf("%d\n", a / b);
+    printf("%d", a % b);
+
+    return 0;
+}
+*/
